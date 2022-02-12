401b033deb547b6a38d6ee1ebafa6b41fbd47d3a
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2022-02-12 22:09:53 +0200

    chore: Cleanup before releasing to the public

--- lib/CognitoGoogleFederationExperiment.ts
+++ lib/CognitoGoogleFederationExperiment.ts
@@ -1,4 +1,4 @@
-import {RemovalPolicy, Stack, StackProps} from "aws-cdk-lib";
+import {Annotations, RemovalPolicy, Stack, StackProps} from "aws-cdk-lib";
 import {
     ProviderAttribute,
     UserPool,
@@ -17,7 +17,19 @@ export class CognitoGoogleFederationExperiment extends Stack {
 
         // Used as a prefix for the Cognito pool domain name as well as some internal SSM Parameter Store parameters
         // Must be globally unique (Cognito limitation) across all AWS accounts (like S3 bucket names)
-        const uniquePrefix = "test-2022";
+        // const uniquePrefix = "test-2022";
+        const uniquePrefix = this.node.tryGetContext('prefix') as string;
+
+        const googleClientId = this.node.tryGetContext('google-client-id') as string;
+        const googleClientSecret = this.node.tryGetContext('google-client-secret') as string;
+
+        if (!uniquePrefix) {
+            Annotations.of(this).addError(`The context variable "prefix" is not defined. It is used as a unique prefix for your Cognito User Pool's Hosted UI domain and a few other things in the infrastructure. It needs to be defined and globally unique (just like S3 bucket names).`);
+        }
+
+        if (!googleClientId || !googleClientSecret) {
+            Annotations.of(this).addError(`The context variable "google-client-id" or "google-client-secret" are not defined`);
+        }
 
         const userPool = new UserPool(this, "UserPool", {
             removalPolicy: RemovalPolicy.DESTROY,
@@ -30,8 +42,8 @@ export class CognitoGoogleFederationExperiment extends Stack {
 
         new UserPoolIdentityProviderGoogle(this, "Google", {
             userPool,
-            clientId: "727537661772-gvsqml5fj15odn7i9ut3q0std5a9fko2.apps.googleusercontent.com",
-            clientSecret: "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",
+            clientId: googleClientId,
+            clientSecret: googleClientSecret,
 
             // Email scope is required, because the default is 'profile' and that doesn't allow Cognito
             // to fetch the user's email from his Google account after the user does an SSO with Google
